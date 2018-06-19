985b995a1fe83c5c425742543de046494a073ad5
emin <emin@Nedims-MacBook-Pro-2.local>
2018-06-19 15:51:19 +0200

    Relation with user and notes

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_19_132133) do
+ActiveRecord::Schema.define(version: 2018_06_19_132614) do
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
