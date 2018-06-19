cb3e9f721681745832fbef0dbecf55818c18f0d8
emin <emin@Nedims-MacBook-Pro-2.local>
2018-06-19 15:02:36 +0200

    Added edit and delete notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -21,14 +21,22 @@ class NotesController < ApplicationController
             render 'new'
         end
     end
-
+    #addind edit action
     def edit
+
     end
 
     def update 
+        if @note.update(note_params)
+            redirect_to @note
+        else
+            render 'edit'
+        end
     end
 
     def destroy 
+        @note.destroy 
+        redirect_to notes_path
     end
 
     private
